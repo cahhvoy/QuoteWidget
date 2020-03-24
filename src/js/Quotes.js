4d25e75710fa795902ac419e78b73b1948652ee9
cahhvoy <cahhvoy@gmail.com>
2020-03-24 13:04:18 +0300

    app minimize and excape buttons

--- src/js/Quotes.js
+++ src/js/Quotes.js
@@ -1,1 +1,29 @@
-console.log("called")
+const remote = require('electron').remote;
+
+document.getElementById("closeBtn").addEventListener('click',()=>{
+    var window = remote.getCurrentWindow();
+    window.close();
+})
+
+document.getElementById("minBtn").addEventListener('click',()=>{
+    var window = remote.getCurrentWindow();
+    window.minimize();
+    // remote.getCurrentWindow().minimize();
+})
+
+document.addEventListener("keydown", event => {
+    switch (event.key) {
+        case "Escape":
+            remote.getCurrentWindow().minimize();
+            break;
+         }
+});
+
+// document.getElementById("max-btn").addEventListener("click", function (e) {
+//     var window = remote.getCurrentWindow();
+//     if (!window.isMaximized()) {
+//         window.maximize();          
+//     } else {
+//         window.unmaximize();
+//     }
+// });
