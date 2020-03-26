2d34cb85c26e5b9325c99d5b8be3444a2eeea2ec
cahhvoy <cahhvoy@gmail.com>
2020-03-26 14:58:45 +0300

    Adding load new Quote Button

--- src/js/Quotes.js
+++ src/js/Quotes.js
@@ -1,5 +1,20 @@
 const remote = require('electron').remote;
 let request = require('request');
+var internetAvailable = require("internet-available");
+
+let isInternetAvailable = false;
+
+checkConnectivity=async ()=>{
+   await internetAvailable({
+       timeout: 5000,
+       retries: 2
+    }).then(() => {
+        return true;
+    }).catch((err) => {
+        return false;
+    });
+}
+
 
 
 document.getElementById("closeBtn").addEventListener('click',()=>{
@@ -21,6 +36,19 @@ document.addEventListener("keydown", event => {
          }
 });
 
+document.getElementById("newQuotebtn").addEventListener('click',()=>{
+        //document.getElementById("newQuotebtn").en
+        
+            document.getElementById("quoteTitle").innerHTML = "<span id=\"errorT\">Loading Quote</span>";
+            let dataH  = document.getElementById("quoteText").innerHTML;
+            document.getElementById("quoteText").innerHTM="";
+            document.getElementById("quoteText").innerHTML="<span class=\"blur\">"+dataH+"</span><div class=\"holder\"><span class=\"dot1\"></span><span class=\"dot2\"></span><span class=\"dot3\"></span><span class=\"dot4\"></span></div>";  
+        loadQuote();
+        
+})
+
+
+
 // document.getElementById("max-btn").addEventListener("click", function (e) {
 //     var window = remote.getCurrentWindow();
 //     if (!window.isMaximized()) {
@@ -29,42 +57,53 @@ document.addEventListener("keydown", event => {
 //         window.unmaximize();
 //     }
 // });
-///tring to solve the problem of the api random not woring
 
-let perPage = 1;
-let page  = Math.floor(Math.random() * 1000)+1; // 1 to  1000
-//let quoteNum = Math.floor(Math.random() * 11)// 0 to  10
 
-var url = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&page="+page+"&per_page="+perPage+"";
+loadQuote= ()=>{
+    isInternetAvailable = checkConnectivity();
+    console.log(isInternetAvailable)
+    if(isInternetAvailable){
+            let perPage = 1;
+            let page  = Math.floor(Math.random() * 1000)+1; // 1 to  1000
+            //let quoteNum = Math.floor(Math.random() * 11)// 0 to  10
 
-request(url,(err,response,body)=>{
-  if(err){
-      document.getElementById("quoteTitle").innerHTML ="<span id=\"errorT\">Error Occured</span>";
-      let dataH  = document.getElementById("quoteText").innerHTML;
-      document.getElementById("quoteText").innerHTML="<span id=\"error\">"+ err+"</span>"+dataH;
-  }
- 
-  let bodyJson = JSON.parse(body);
-  if(response["statusCode"]==200){
-        //   let quoteId = bodyJson[0]['id'];
-        //   let quoteGuid = bodyJson[0]['guid'];
-        //   let quotesLug = bodyJson[0]['slug'];
-        //   let quoteLink = bodyJson[0]['link'];
-        let quoteTitle = bodyJson[0]["title"];
-        let quoteContent = bodyJson[0]["content"];
+            var url = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&page="+page+"&per_page="+perPage+"";
 
-        document.getElementById("quoteTitle").innerHTML = quoteTitle['rendered'];
-        document.getElementById("quoteText").innerHTML = quoteContent['rendered'];
-  }else if(response["statusCode"]==400){
-        document.getElementById("quoteTitle").innerHTML = "<span id=\"errorT\">Error Occurred</span>";
-        let dataH  = document.getElementById("quoteText").innerHTML;
-        document.getElementById("quoteText").innerHTML="<span id=\"error\">"+bodyJson["message"] +"</span>"+dataH;        
-  }else{
-      //standard Error here
-      document.getElementById("quoteTitle").innerHTML ="<span id=\"errorT\">Error</span>";
-      let dataH  = document.getElementById("quoteText").innerHTML;
-      document.getElementById("quoteText").innerHTML="<span id=\"error\"> an unknown Error Occured </span>"+dataH;
-  }
+            request(url,(err,response,body)=>{
+            if(err){
+                document.getElementById("quoteTitle").innerHTML ="<span id=\"errorT\">Error Occured</span>";
+                let dataH  = document.getElementById("quoteText").innerHTML;
+                document.getElementById("quoteText").innerHTML="<span id=\"error\">"+ err+"</span>"+dataH;
+            }
+            
+            let bodyJson = JSON.parse(body);
+            if(response["statusCode"]==200){
+                    //   let quoteId = bodyJson[0]['id'];
+                    //   let quoteGuid = bodyJson[0]['guid'];
+                    //   let quotesLug = bodyJson[0]['slug'];
+                    //   let quoteLink = bodyJson[0]['link'];
+                    let quoteTitle = bodyJson[0]["title"];
+                    let quoteContent = bodyJson[0]["content"];
 
-  //console.log(bodyJson)
-})
+                    document.getElementById("quoteTitle").innerHTML = quoteTitle['rendered'];
+                    document.getElementById("quoteText").innerHTML = quoteContent['rendered'];
+            }else if(response["statusCode"]==400){
+                    document.getElementById("quoteTitle").innerHTML = "<span id=\"errorT\">Error Occurred</span>";
+                    let dataH  = document.getElementById("quoteText").innerHTML;
+                    document.getElementById("quoteText").innerHTML="<span id=\"error\">"+bodyJson["message"] +"</span>"+dataH;        
+            }else{
+                //standard Error here
+                document.getElementById("quoteTitle").innerHTML ="<span id=\"errorT\">Error</span>";
+                let dataH  = document.getElementById("quoteText").innerHTML;
+                document.getElementById("quoteText").innerHTML="<span id=\"error\"> an unknown Error Occured </span>"+dataH;
+            }        
+
+   
+       })
+    }else{
+        document.getElementById("quoteTitle").innerHTML = "No Internet";
+        document.getElementById("quoteText").innerHTML="<span class=\"blur\">Connection Error</span><div class=\"holder\"><span class=\"dot1\"></span><span class=\"dot2\"></span><span class=\"dot3\"></span><span class=\"dot4\"></span></div>";  
+    }
+}
+
+loadQuote();
