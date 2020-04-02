8e2ad0582257d492d5bf43d2ed130105d5bcc599
cahhvoy <cahhvoy@gmail.com>
2020-04-02 18:01:45 +0300

    added sqlite {accessed using knexjs}

--- src/js/Quotes.js
+++ src/js/Quotes.js
@@ -2,8 +2,74 @@ const remote = require('electron').remote;
 let request = require('request');
 var internetAvailable = require("internet-available");
 
-let isInternetAvailable = false;
+const path = require('path');
 
+var knexConn = require("knex")({
+	client: "sqlite3",
+	connection: {
+		filename: path.join( __dirname,"../Db/QuotesDB.db")
+    },
+    useNullAsDefault: true
+});
+
+
+saveQuote = (author,Quote)=>{
+  //insert quote into db 
+  let Qdate = new Date();
+  
+  //"quoteAuthor", "quoteBody", "quoteDate", "onlineLink", "quoteSource", "quoteSourceId"
+knexConn.insert({'quoteAuthor':author,'quoteBody':Quote,'quoteDate':Qdate}).into('Quotes')
+// .then( function (result) {
+//     res.json({ success: true, message: 'ok' });
+//  })
+.then((id)=>{
+   //console.log(id[0])
+      console.log("ok "+id)
+   return true;
+ })
+ .catch((err)=>{
+     console.log("err "+err)
+   return false;
+ })
+}
+
+
+isQuoteSaved = async(Quote)=>{
+    Quote= Quote.trim();
+    let saved=true;
+     await knexConn('Quotes').where('quoteBody', 'like', '%'+Quote+'%')
+    .then((resp)=>{
+         saved = resp.length==0?false:true
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    return saved;
+}
+
+readQuote = ()=>{
+// get only one random quote
+//knexConn.select('quoteAuthor','quoteBody').from('Quotes')
+}
+
+readAllQuote = ()=>{
+//retun all rowns // limit included
+}
+
+totalSavedQuotes = async ()=>{
+    tot=0;
+        await knexConn('Quotes').count('quoteBody', {as: 'rows'})
+        .then((res)=>{
+            tot = JSON.stringify(res[0]['rows']);
+        })
+        .catch((err)=>{
+            tot = 0;
+        })
+  return tot
+}
+
+let isInternetAvailable = false;
+let totalSaved = 0;
 var MainSetInt="";
 var ReloadSetInt="";
 var reloadInterval= 300000;
@@ -80,8 +146,15 @@ loadQuote = async ()=>{
             let page  = Math.floor(Math.random() * 1000)+1;
             
             var url = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&page="+page+"&per_page="+perPage+"";
+            
+            var url2="https://quotes.rest/quote/random?language=en&limit=1"
+            request(url2,(err,response,body)=>{
+                console.log(JSON.stringify(err))
+                console.log(JSON.stringify(response))
+                console.log(JSON.stringify(body))
 
-            request(url,(err,response,body)=>{
+            })
+            request(url,async(err,response,body)=>{
             if(err){
                 document.getElementById("quoteTitle").innerHTML ="<span id=\"errorT\">Error Occured</span>";
                 document.getElementById("quoteText").innerHTML="";
@@ -89,17 +162,30 @@ loadQuote = async ()=>{
             }
             
             let bodyJson = JSON.parse(body);
+          
             if(response["statusCode"]==200){
+               
                     //   let quoteId = bodyJson[0]['id'];
                     //   let quoteGuid = bodyJson[0]['guid'];
                     //   let quotesLug = bodyJson[0]['slug'];
                     //   let quoteLink = bodyJson[0]['link'];
                     let quoteTitle = bodyJson[0]["title"];
                     let quoteContent = bodyJson[0]["content"];
-                    
+                    //check if saved
+                    let _PTrimed = quoteContent['rendered'].replace(/<[^>]+>/gm, '').trim()
+                    var isSaved = await isQuoteSaved(_PTrimed);
+            
+                    if(!isSaved){
+                        //save to Db
+                        saveQuote(quoteTitle['rendered'],quoteContent['rendered']);
+                    }
+                    //get total saved
+                    totalSaved = await totalSavedQuotes();
+
                     document.getElementById("quoteTitle").innerHTML = quoteTitle['rendered'];
-                    document.getElementById("quoteText").innerHTML="";
-                    document.getElementById("quoteText").innerHTML = quoteContent['rendered'];
+                    document.getElementById("quoteText").innerHTML="";   
+                    document.getElementById("quoteText").innerHTML = _PTrimed;
+                    document.getElementById("totSavedA").innerHTML = totalSaved
             }else if(response["statusCode"]==400){
                     document.getElementById("quoteTitle").innerHTML = "<span id=\"errorT\">Error Occurred</span>";
                     let dataH  = document.getElementById("quoteText").innerHTML;
